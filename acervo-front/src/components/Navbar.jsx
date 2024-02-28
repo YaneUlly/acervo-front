a49e89ea792a5320c408c7307b9311baf6863cc6
Yane Ully <yaneully@gmail.com>
2024-02-28 19:39:25 +0000

    created the env file and set up the navbar

--- acervo-front/src/components/Navbar.jsx
+++ acervo-front/src/components/Navbar.jsx
@@ -1,5 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/howtotaste'>How to taste</Link>
+      <Link to='coffeequiz'>Coffee Quiz</Link>
+    </nav>
+  );
 }
 
 export default Navbar;
